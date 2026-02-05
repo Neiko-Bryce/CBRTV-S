c1752ed9b4817e45e410ecc9581ed7831c7eea1c
Neiko-Bryce <neikobrycefantilaga@gmail.com>
2026-02-06 01:34:10 +0800

    urlstudentlogo

--- resources/views/student/vote.blade.php
+++ resources/views/student/vote.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Vote - {{ $election->election_name }} - CpsuVotewisely.com</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800&family=playfair-display:400,600,700" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
